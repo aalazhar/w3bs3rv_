c2d2a5ca6688f8d6b83c6c08f073040f0fcf74f4
Anas Alazhari <aalazhar@e1r3p8.1337.ma>
2023-06-03 18:43:51 +0100

    add session files

---
+++ UTILS/validat.php
@@ -0,0 +1,31 @@
+<?php
+    ob_start();
+    $email = "aalazhar@42.com";
+    $pass = "123456";
+
+    if (isset($_POST['login']) && isset($_POST['email']) && isset($_POST['pass'])){
+        $emailX = $_POST['email'];
+        $passX = $_POST['pass'];
+        $loginX = $_POST['login'];
+
+        if ($email == $emailX && $pass == $passX){
+            session_start();
+            $_SESSION['login'] = $loginX;
+            $_SESSION['email'] = $emailX;
+            $_SESSION['pass'] = $passX;
+            header ("location: welcom.php");
+        }
+        else {
+            echo "Your email or pass is invalid. Click here <a href='login.php' >to try again.</a>";
+
+        }
+    }
+    else{
+        header("location : login.php");
+    }
+?>
+<style type="text/css">
+    * {
+        font-family: "Courier new";
+    }
+</style>
